c6e8314fa351977bd58a5cebd9379db65cd6eeec
EC2 Default User <ec2-user@ip-172-31-23-120.us-east-2.compute.internal>
2018-03-07 17:39:29 +0000

    wtf movies??

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,22 +11,62 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #citations
+    # sources
+    # https://teamtreehouse.com/community/ruby-on-rails-params-hash-explained
     # https://stackoverflow.com/questions/9646815/conditionally-setting-css-style-from-ruby-controller
     # https://apidock.com/ruby/Array/uniq
     # http://mitrev.net/ruby/2015/11/13/the-operator-in-ruby/
     # https://stackoverflow.com/questions/12084507/what-does-the-map-method-do-in-ruby
     # https://stackoverflow.com/questions/5267950/how-to-use-the-where-method-from-activerecordquerymethods-in-order-to-limi
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    @checked = if params[:ratings] then params[:ratings].keys else @all_ratings end
-    @checked.each do |rating|
-      params[rating] = true
-    end
+    # http://matthewcarriere.com/2008/06/23/using-select-reject-collect-inject-and-detect/
+    # https://www.lynda.com/Ruby-Rails-tutorials/Ruby-Rails-5-Essential-Training/500551-2.html
+    
+    #redirect flag. If signalled, it will use flash.keep and redirect.
+    @redirect = false
 
+    #original init of movies and checkboxes. Still works, I think?
     if params[:sort]
       @movies = Movie.order(params[:sort])
     else
       @movies = Movie.where(:rating => @checked)
+    end
+    
+    #current session override for sorting
+    if(params[:sort] == 'title')
+      session[:sort] = params[:sort]
+      @movies = @movies.sort_by{|x| x.title }
+    elsif(params[:sort] == 'release_date')
+      session[:sort] = params[:sort]
+      @movies = @movies.sort_by{|x| x.release_date }
+    elsif(session.key?(:sort) )
+      params[:sort] = session[:sort]
+      @redirect = true
+    end
+    
+    #current session override for ratings
+    if(params[:ratings] != nil)
+      session[:ratings] = params[:ratings]
+      @movies = @movies.select{ |x| params[:ratings].key?(x.rating) }
+    elsif(session.key?(:ratings) )
+      params[:ratings] = session[:ratings]
+      @redirect = true
+    end
+    
+    if @redirect == true
+      flash.keep
+      redirect_to movies_path(:sort => params[:sort], :ratings => params[:ratings] )
+    end
+
+    # reinitialize checked and the all ratings. I'm not sure why, but this code fixed an issue
+    @all_ratings =  ['G','PG','PG-13','R']
+    @checked = {}
+
+    # After session
+    if @checked != nil
+      @checked = if params[:ratings] then params[:ratings].keys else @all_ratings end
+      @checked.each do |rating|
+        params[rating] = true
+      end
     end
   end
 
